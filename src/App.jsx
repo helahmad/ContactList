382254ac66fd0c5bdd0aec855c9d35cefd389197
helahmad <ahmadmhelmi@gmail.com>
2023-08-18 19:08:42 -0400

    final

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,22 @@
 
-import './App.css'
+import "./App.css";
+import { useState } from "react";
+import ContactList from "./components/ContactList.jsx";
+import SelectedContact from "./components/SelectedContact.jsx";
 
-function App() {
-  
+export default function App() {
+  const [selectedContactId, setSelectedContactId] = useState(null);
 
   return (
     <>
-     
+      {selectedContactId ? (
+        <SelectedContact
+          selectedContactId={selectedContactId}
+          setSelectedContactId={setSelectedContactId}
+        />
+      ) : (
+        <ContactList setSelectedContactId={setSelectedContactId} />
+      )}
     </>
-  )
+  );
 }
-
-export default App
